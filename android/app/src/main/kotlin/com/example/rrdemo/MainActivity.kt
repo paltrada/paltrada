a86e2499c48164bcaa0d3fccfb7f1cff717d23a4
sai <paltrada56@gmail.com>
2024-05-14 05:48:29 -0700

    new project

---
+++ android/app/src/main/kotlin/com/example/rrdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rrdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/myhome.dart
@@ -0,0 +1,96 @@
+
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class SMSScreen extends StatefulWidget {
+  const SMSScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SMSScreen> createState() => _SMSScreenState();
+}
+
+class _SMSScreenState extends State<SMSScreen> {
+  List<Contact> contacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _askPermissions("");
+  }
+
+  Future<void> _askPermissions(String routeName) async {
+    PermissionStatus permissionStatus = await _getContactPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      if (routeName != null) {
+        MyContact();
+      }
+    } else {
+      _handleInvalidPermissions(permissionStatus);
+    }
+  }
+
+  Future<PermissionStatus> _getContactPermission() async {
+    PermissionStatus permission = await Permission.contacts.status;
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.permanentlyDenied) {
+      PermissionStatus permissionStatus = await Permission.contacts.request();
+      return permissionStatus;
+    } else {
+      return permission;
+    }
+  }
+
+  void _handleInvalidPermissions(PermissionStatus permissionStatus) {
+    if (permissionStatus == PermissionStatus.denied) {
+      final snackBar = SnackBar(content: Text('Access to contact data denied'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else if (permissionStatus == PermissionStatus.permanentlyDenied) {
+      final snackBar =
+      SnackBar(content: Text('Contact data not available on device'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
+  void MyContact()async{
+    contacts = await ContactsService.getContacts(withThumbnails: false);
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contacts"),
+      ),
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          Contact c = contacts[index];
+          String name = " ";
+          if(c.displayName != null){
+            name = c.displayName!;
+          }
+
+          String phoneNo = " ";
+          if(c.phones != null && c.phones!.isNotEmpty){
+            Item item = c.phones![0];
+            if(item.value != null){
+              phoneNo = item.value!;
+            }
+          }
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(child: Text(name)),
+              Text(phoneNo),
+            ],
+          );
+
+        },),
+    );
+  }
+}
